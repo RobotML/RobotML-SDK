6109bc8939ce8796dc9d39e9f487284002f6727b
Seb <seb@L004.(none)>
2012-10-18 12:01:20 +0200

    [aroccam] initial commit to github

---
+++ plugins/generators/aroccam/org.eclipse.proteus.generators.acceleo.aroccam.ui/src/org/eclipse/proteus/generators/acceleo/aroccam/ui/handler/AcceleoAroccamCodeGenerator.java
@@ -0,0 +1,80 @@
+/*****************************************************************************
+ * Copyright (c) 2011 CEA LIST.
+ *
+ *    
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Saadia DHOUIB (CEA LIST) - Initial API and implementation
+ *
+ *****************************************************************************/
+package org.eclipse.proteus.generators.acceleo.aroccam.ui.handler;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.papyrus.ui.toolbox.notification.Type;
+import org.eclipse.papyrus.ui.toolbox.notification.builders.CombinedPopupAndViewBuilder;
+import org.eclipse.papyrus.ui.toolbox.notification.builders.NotificationBuilder;
+import org.eclipse.papyrus.widgets.util.FileUtil;
+import org.eclipse.proteus.generators.acceleo.aroccam.Generate_aroccam;
+
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class AcceleoAroccamCodeGenerator.
+ */
+public class AcceleoAroccamCodeGenerator {
+
+	/** The input model for the acceleo generator. */
+	private EObject generationInputModel;
+
+	/** the aroccam source folder. */
+	private File aroccamTargetFolder;
+
+	/**
+	 * Instantiates a new acceleo rt maps code generator.
+	 */
+	public AcceleoAroccamCodeGenerator() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * Run acceleo transformation.
+	 * 
+	 * @param generationInputModel
+	 *        the generation input model
+	 * @param aroccamTargetFolderPath
+	 *        the aroccam target folder path
+	 */
+	public void runAcceleoTransformation(EObject generationInputModel, String aroccamTargetFolderPath) {
+		this.generationInputModel = generationInputModel;
+		this.aroccamTargetFolder = new File(aroccamTargetFolderPath);
+		if(this.generationInputModel != null && this.aroccamTargetFolder != null) {
+			try {
+
+				File targetFolder = FileUtil.getWorkspaceFile(aroccamTargetFolderPath); //$NON-NLS-1$ //$NON-NLS-2$
+				if(!targetFolder.getParentFile().exists()) {
+					targetFolder.getParentFile().mkdirs();
+				}
+				//Instantiate a new acceleo generator 
+				Generate_aroccam generate = new Generate_aroccam(this.generationInputModel, targetFolder, Collections.EMPTY_LIST);
+
+				generate.doGenerate(null);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
+			new NotificationBuilder().setBuilderClass(CombinedPopupAndViewBuilder.class).setType(Type.ERROR).setTitle("Acceleo genration error").setMessage("You have to set the parameters of the generation").run();
+		}
+
+	}
+
+}

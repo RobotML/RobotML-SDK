655565e8c74bdc19ee19230574cac27cf7b85bc8
Clément Fouque <clement.fouque@lasmea.univ-bpclermont.fr>
2012-11-16 11:15:31 +0100

    Updating package name

--- installer/linux/ubuntu-12.04/metapackages/pkg.sh
+++ installer/linux/ubuntu-12.04/metapackages/pkg.sh
@@ -2,7 +2,8 @@
 
 VERSION=0.1
 DIR_PKG=$(pwd)
-REP_PKG=/media/data/work/repo
+REP_PKG=${DIR_PKG}/repo
+REP_DEB=${DIR_PKG}/deb
 ALL_PKG="proteus-*"
 
 echo "==== init  ===="
@@ -15,24 +16,26 @@ fi
 
 echo "${pkgs}"
 
-DIR_TMP=$(mktemp -d)
-cp -r . ${DIR_TMP}
-cd ${DIR_TMP}
-# clean SVN cache
-find . -name .svn | xargs rm -rf
 
 echo "==== pack  ===="
 
 for p in ${pkgs}; do
     if [ -d "${p}" ]; then
-        DEB_PKG="${p}_${VERSION}_i386.deb"
-        #[ -e ${DEB_PKG} ] && mv ${DEB_PKG} ${DEB_PKG}.$(date +%s).bak
-        fakeroot dpkg -b ${p} ${DEB_PKG}
-        reprepro -Vb ${REP_PKG} remove stable ${p}
-        reprepro -S main -P optional -Vb ${REP_PKG} includedeb stable ${DEB_PKG}
+	chmod 0775 ${p}
+        DEB_PKG="${REP_DEB}/${p}_${VERSION}_i386.deb"
+        dpkg-deb --build ${p} ${DEB_PKG}
+        reprepro -b ${REP_PKG} remove stable ${p}
     fi
 done
 
+dpkg-deb --build eclipse-proteus deb/eclipse-proteus_0.1_i386.deb
+
+echo "==== additional package ======"
+
+for f in ${REP_DEB}/*.deb
+do
+  reprepro -S main -P optional -b ${REP_PKG} includedeb stable ${f}
+done
 echo "==== clean ===="
 
 cd ${DIR_PKG}

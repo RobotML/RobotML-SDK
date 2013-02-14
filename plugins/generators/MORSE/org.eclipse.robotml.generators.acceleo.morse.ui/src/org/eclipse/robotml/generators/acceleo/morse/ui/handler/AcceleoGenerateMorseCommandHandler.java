52c074aa92747406723a5f5ddf3a0e53734a2a78
Serge Stinckwich <Serge.Stinckwich@gmail.com>
2013-02-14 18:00:29 +0100

    The last modification for #84

---
+++ plugins/generators/MORSE/org.eclipse.robotml.generators.acceleo.morse.ui/src/org/eclipse/robotml/generators/acceleo/morse/ui/handler/AcceleoGenerateMorseCommandHandler.java
@@ -0,0 +1,50 @@
+/*****************************************************************************
+ * Copyright (c) 2011 CEA LIST.
+ *
+ *    
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Saadia DHOUIB (CEA LIST) - Initial API and implementation
+ *
+ *****************************************************************************/
+package org.eclipse.robotml.generators.acceleo.morse.ui.handler;
+
+import org.eclipse.emf.common.command.Command;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
+import org.eclipse.papyrus.commands.wrappers.GMFtoEMFCommandWrapper;
+import org.eclipse.papyrus.views.modelexplorer.handler.AbstractCommandHandler;
+
+/**
+ * The Class AcceleoGenerateMorseCommandHandler.
+ */
+public class AcceleoGenerateMorseCommandHandler extends AbstractCommandHandler {
+
+	/** The morse folder. */
+	private final String morseFolder = "/morse-generated-files";
+
+	/**
+	 * @see org.eclipse.papyrus.modelexplorer.handler.AbstractCommandHandler#getCommand()
+	 * 
+	 * @return
+	 */
+
+	@Override
+	protected Command getCommand() {
+		EObject selectedObject = getSelectedElement();
+		TransactionalEditingDomain editingDomain = getEditingDomain();
+
+		URI targetFolderURI = selectedObject.eResource().getURI();
+		int lastindex = targetFolderURI.toPlatformString(false).lastIndexOf("/");
+		String targetPath = targetFolderURI.toPlatformString(false).substring(0, lastindex);
+
+		return new GMFtoEMFCommandWrapper(new GenerateMorseCodeCommand("Generate Morse code command", editingDomain, selectedObject, targetPath + morseFolder));
+
+	}
+
+}

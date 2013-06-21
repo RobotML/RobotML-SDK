873d0c8f288c90d74221225a7f3487643ea1a6ea
sdhouib <dhouib@gmail.com>
2013-06-21 09:50:58 +0200

    First commit of oeg.eclipse.papyrus.robotml.deployment

---
+++ robotml-kepler/dsl-papyrus/robotml/org.eclipse.papyrus.robotml.deployment/src/org/eclipse/papyrus/robotml/deployment/TransformationException.java
@@ -0,0 +1,11 @@
+package org.eclipse.papyrus.robotml.deployment;
+
+public class TransformationException extends Exception {
+
+	public TransformationException(String reason) {
+		super(reason);
+	}
+
+	// String m_reason;
+	final static long serialVersionUID = 1234;
+}

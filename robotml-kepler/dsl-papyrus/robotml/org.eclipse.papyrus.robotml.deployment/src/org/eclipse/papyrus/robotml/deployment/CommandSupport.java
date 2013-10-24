6a2112e282b937d1056c9a0574a61653152690b7
sdhouib <dhouib@gmail.com>
2013-10-24 11:45:01 +0200

    respecting papyrus coding styles

--- robotml-kepler/dsl-papyrus/robotml/org.eclipse.papyrus.robotml.deployment/src/org/eclipse/papyrus/robotml/deployment/CommandSupport.java
+++ robotml-kepler/dsl-papyrus/robotml/org.eclipse.papyrus.robotml.deployment/src/org/eclipse/papyrus/robotml/deployment/CommandSupport.java
@@ -1,13 +1,14 @@
-/**
- * Copyright CEA-LIST 2012
- * available under EPL 1.0 licence
- * 
- * This file is part of the Qompass tool chain (www.qompass.net)
- * 
+/*****************************************************************************
+ * Copyright (c) 2013 CEA LIST.
+ *    
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
- * $Id$
- */
+ * Contributors:
+ *  Saadia Dhouib (CEA LIST) saadia.dhouib@cea.fr - Initial API and implementation
+ *****************************************************************************/
 
 package org.eclipse.papyrus.robotml.deployment;
 
--- robotml-kepler/dsl-papyrus/robotml/org.eclipse.papyrus.robotml.deployment/src/org/eclipse/papyrus/robotml/deployment/Copy.java
+++ robotml-kepler/dsl-papyrus/robotml/org.eclipse.papyrus.robotml.deployment/src/org/eclipse/papyrus/robotml/deployment/Copy.java
@@ -1,13 +1,13 @@
 /*****************************************************************************
- * Copyright (c) 2012 CEA LIST.
- *
+ * Copyright (c) 2013 CEA LIST.
  *    
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the CeCILL-C Free Software License v1.0
+ * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- *
+ * Contributors:
+ *  Saadia Dhouib (CEA LIST) saadia.dhouib@cea.fr - Initial API and implementation
  *****************************************************************************/
 package org.eclipse.papyrus.robotml.deployment;
 
